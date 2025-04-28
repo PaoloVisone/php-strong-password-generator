1b6b213b47c865b72171f3d3c9111880a501e8ba
PaoloVisone <polvis.pv.22@gmail.com>
2025-04-28 20:05:19 +0200

    Refactor password generation logic into a single function with customizable options

--- function.php
+++ function.php
@@ -1,19 +1,43 @@
-<!-- Funzione di stringhe random -->
 <?php
-function generateRandomString($length = 10)
-{
-    // Set di caratteri
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#%&*+-=?[]{}';
-    $charactersLength = strlen($characters);
-    $randomString = '';
 
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[random_int(0, $charactersLength - 1)];
+// Funzione genera password 
+function generatePassword($lunghezza, $ripetizione, $includeLetters, $includeNumbers, $includeSymbols)
+{
+    // Variabile in cui inseriamo i caratteri
+    $caratteri = '';
+
+    // Se l'utente seleziona lettere, numeri o simboli
+    if ($includeLetters) {
+        $caratteri .= 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+    if ($includeNumbers) {
+        $caratteri .= '0123456789';
+    }
+    if ($includeSymbols) {
+        $caratteri .= '!@#%&*+-=?[]{}';
     }
 
-    return $randomString;
-}
+    if ($caratteri === '') {
+        $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    }
 
-// Echo the random string.
-echo generateRandomString();
-?>
+    $password = '';
+
+    // Se l'utente seleziona la ripetizione
+    if ($ripetizione) {
+        for ($i = 0; $i < $lunghezza; $i++) {
+            // Genera un numero casuale compreso tra 0 e l'ultimo indice possibile
+            $password .= $caratteri[random_int(0, strlen($caratteri) - 1)];
+        }
+    } else {
+        // In caso di errori
+        if ($lunghezza > strlen($caratteri)) {
+            return 'Errore: troppi caratteri richiesti senza ripetizione.';
+        } else {
+            // Prendo i caratteri disponibili, li mescolo e prendo solo i primi che mi servono per creare la password
+            $password = substr(str_shuffle($caratteri), 0, $lunghezza);
+        }
+    }
+
+    return $password;
+}
